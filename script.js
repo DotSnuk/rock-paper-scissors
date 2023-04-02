9607a521ff300f3acdc800ffb49262a0c2338597
DotSnuk <hampusfarner@outlook.com>
2023-04-02 14:22:58 +0200

    random computer choice function added

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function getComputerChoice(){
     const rndNumber = Math.ceil(Math.random() * 3);
-    console.log(rndNumber);
+    let computerChoice;
+    switch(rndNumber){
+        case 1:
+            computerChoice = 'rock';
+            break;
+        case 2:
+            computerChoice = 'paper';
+            break;
+        case 3:
+            computerChoice = 'scissors';
+            break;
+    }
+    console.log(computerChoice);
 }
 getComputerChoice();
