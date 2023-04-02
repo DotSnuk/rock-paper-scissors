84de64a104a0d013558a10c1e310bc1b1c995e29
DotSnuk <hampusfarner@outlook.com>
2023-04-02 14:54:46 +0200

    Function for playing one round created. For hard coded user choice

--- script.js
+++ script.js
@@ -13,18 +13,34 @@ function getComputerChoice(){
             break;
     }
     console.log(computerChoice);
+    return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection){
-        return ("Draw! Both picked" + playerSelection);
+        return ("Draw! Both picked " + playerSelection);
     } else if (playerSelection === 'rock'){
         if (computerSelection === 'paper'){
             return "You lose! Paper beats rock";
         } else {
-            return "You win! Rock beats scissors!"
+            return "You win! Rock beats scissors"
+        }
+    } else if (playerSelection === 'paper'){
+        if (computerSelection === 'scissors'){
+            return "You lose! Scissors beats paper";
+        } else {
+            return "You win! Paper beats rock"
+        }
+    } else if (playerSelection === 'scissors'){
+        if (computerSelection === 'rock'){
+            return "You lose! Rock beats scissors";
+        } else {
+            return "You win! Scissors beats paper";
         }
     }
 }
-getComputerChoice();
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
